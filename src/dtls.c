cb15a577ebb690e8a52a4b3e16b8072eaac3e0a3
Andrea Vouk <andrew.vouk@gmail.com>
2023-01-17 10:29:05 +0100

    Port Forzato level 1 di sicurezza per DTLS (datachannel)
    commit b00b9f8431b95c3c78cbdac0abbf4976a949262a

--- src/dtls.c
+++ src/dtls.c
@@ -445,6 +445,15 @@ gint janus_dtls_srtp_init(const char *server_pem, const char *server_key, const 
 		"SRTP_AES128_CM_SHA1_80:SRTP_AES128_CM_SHA1_32");
 #endif
 
+    /* 30/06/2022 - OpenSSL su Bullseye imposta per default level 2, quindi almeno
+       112 bit di sicurezza, ma la app ne offre 80 (level 1). Forzo quindi il
+       level 1 per questo context e sbloccare la situazione. Altrimenti il datachannel
+       non si apre. */
+    /* FIXME: TLSv1 has been long deprecated and no longer supported by default in many reverse proxy.
+     *        Idealmente, passare a TLSv1.3 come default con fallback a TLSv1.2 e TLSv1.1
+     */
+    SSL_CTX_set_security_level(ssl_ctx, 1);
+
 	if(!server_pem && !server_key) {
 		JANUS_LOG(LOG_INFO, "No cert/key specified, autogenerating some...\n");
 		if(janus_dtls_generate_keys(&ssl_cert, &ssl_key, rsa_private_key) != 0) {
